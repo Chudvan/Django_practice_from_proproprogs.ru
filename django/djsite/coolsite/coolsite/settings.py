a56050e1b35da1acf26f03d6ed9c2fcf5ad32199
ivan <Mr.Chudvan@yandex.ru>
2022-03-13 16:26:24 +0300

    lesson-5 until paragraph 'Менеджер записей objects'

--- django/djsite/coolsite/coolsite/settings.py
+++ django/djsite/coolsite/coolsite/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-#ix1-8j5hz-37zt#bz&_163^z)8p*%xyw$7mjg0yxc@=*6+o$t'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1']
 
