ee0e5c48d44fb3e63a09047e8bbf06535224c6bd
ivan <Mr.Chudvan@yandex.ru>
2022-05-22 01:29:01 +0300

    end of the lesson-6

--- django/djsite/coolsite/women/urls.py
+++ django/djsite/coolsite/women/urls.py
@@ -4,6 +4,5 @@ from .views import *
 
 urlpatterns = [
     path('', index, name='home'),
-    path('cats/<slug:cat>/', categories, name='cats'),
-    re_path(r'^archive/(?P<year>[0-9]{4})/', archive, name='archive')
+    path('about/', about, name='about')
 ]
--- django/djsite/coolsite/women/views.py
+++ django/djsite/coolsite/women/views.py
@@ -1,10 +1,18 @@
-from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound, Http404
-from django.shortcuts import redirect
+from django.shortcuts import redirect, render
+from .models import *
+
+
+menu = ["О сайте", "Добавить статью", "Обратная связь", "Войти"]
 
 
 def index(request):
-    return HttpResponse("Страница приложения women.")
+    posts = Women.objects.all()
+    return render(request, 'women/index.html',  {'posts': posts, 'menu': menu, 'title': 'Главная страница'})
+
+
+def about(request):
+    return render(request, 'women/about.html', {'menu': menu,'title': 'О сайте'})
 
 
 def categories(request, cat):
